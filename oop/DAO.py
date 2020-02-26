ca54a31afe8dab3ab8e3d21d222e42761aef57fa
Hernán Alperin <alpe@alum.mit.edu>
2020-02-26 11:21:51 -0300

    inicio de DAO

---
+++ oop/DAO.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+título: DAO.py
+descripción: Objeto Abstacto de Datos
+envuelve la comunicación entre el código los datos
+usando classes
+fecha: 2020-02-26
+autor: -h
+"""
+from operator import *
+import logging
+
+class DAO:
+    # Data Abract Object
+
+    def __init__(db_string):
+        conn_info = sb
+
+    def __str__(self):
+        return str((self.c_id, self.vivs))
+
+
+
