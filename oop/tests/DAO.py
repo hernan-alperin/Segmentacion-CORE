deb226662626ab67f72c4124cc67eab8d537a029
Hernán Alperin <alpe@alum.mit.edu>
2020-02-26 16:07:24 -0300

    esbozo de tests DAO

---
+++ oop/tests/DAO.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+import logging
+logging.basicConfig(filename='unittest.log',
+                    format='%(asctime)s %(levelname)s:%(message)s',
+                    datefmt='%d/%m/%Y %I:%M:%S %p',
+                    level=logging.DEBUG
+                    )
+
+class TestDAO(unittest.TestCase):
+
+    def test_connection(self):
+        censo == DAO('')
+
+
