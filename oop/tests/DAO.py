1bdc01c6d5306c609e5a1f17e3c4e083efb470f3
Hernán Alperin <alpe@alum.mit.edu>
2020-02-27 10:19:11 -0300

    tratnado de hacer andar el testeo...

--- oop/tests/DAO.py
+++ oop/tests/DAO.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 import logging
+import psycopg2
+from .. import DAO # esto no anda... ver como importar el modulo
+
 logging.basicConfig(filename='unittest.log',
                     format='%(asctime)s %(levelname)s:%(message)s',
                     datefmt='%d/%m/%Y %I:%M:%S %p',
@@ -8,8 +11,14 @@ logging.basicConfig(filename='unittest.log',
                     )
 
 class TestDAO(unittest.TestCase):
-
+    dao = DAO()
     def test_connection(self):
-        censo == DAO('')
+        self.assertRaises(psycopg2.Error, dao.db, '')
+        self.assertRaises(psycopg2.Error, DAO.__init__, 'a')
+        self.assertRaises(psycopg2.Error, DAO.__init__, 'a:b:c')
+        self.assertRaises(psycopg2.Error, DAO.__init__, 'segmentador:rodatnemges:censo2020:172.26.67.239')
+
+if __name__ == '__main__':
+    unittest.main()
 
 
