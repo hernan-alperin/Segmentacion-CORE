f467a2641e38ee8f80f3bf8b9e074ebc0476f242
Hernán Alperin <hernan.alperin@gmail.com>
2021-12-01 17:06:10 -0300

    retorna cuantas

--- editar_segmentacion/asignar_viviendas_a_segmento.sql
+++ editar_segmentacion/asignar_viviendas_a_segmento.sql
@@ -2,13 +2,21 @@
 SELECT indec.asignar_viviendas_a_segmento('e0960', 'ARRAY [594, 595, 596]', 10006);
 */
 
+drop indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint);
 CREATE or replace FUNCTION indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint) 
-RETURNS VOID AS $$ 
+RETURNS integer AS $$ 
+declare cuantas integer;
 begin
+execute '
+select count(*)
+where listado_id = any (' || viviendas || ')
+' into cuantas;
+
 execute '
 update ' || esquema || '.segmentacion 
 set segmento_id = ''' || segmento_id || '''
 where listado_id = any (' || viviendas || ')
 ';
-return;
+
+return cuantas;
 END $$ LANGUAGE plpgsql;
