d6b673eec362c5572ca94c34f6956128fa579455
Hernán Alperin <alpe@alum.mit.edu>
2021-12-01 16:32:01 -0300

    testeada local y anda

--- editar_segmentacion/asignar_viviendas_a_segmento.sql
+++ editar_segmentacion/asignar_viviendas_a_segmento.sql
@@ -1,9 +1,14 @@
-CREATE FUNCTION indec.asignar_viviendas_a_segmento(esquema test, vivendas integer[], segmento_id bigint) RETURNS VOID AS $$ 
+/*
+SELECT indec.asignar_viviendas_a_segmento('e0960', 'ARRAY [594, 595, 596]', 10006);
+*/
+
+CREATE or replace FUNCTION indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint) 
+RETURNS VOID AS $$ 
 begin
 execute '
 update ' || esquema || '.segmentacion 
 set segmento_id = ''' || segmento_id || '''
-where listado_id = any (viviendas)
+where listado_id = any (' || viviendas || ')
 ';
-return
+return;
 END $$ LANGUAGE plpgsql;
