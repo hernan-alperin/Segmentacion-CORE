71fcc8ae041567be8d90617b1fe48517f82de5ee
Hernán Alperin <alpe@alum.mit.edu>
2021-12-01 17:08:49 -0300

    probada, retorna cuantas viviendas asignó

--- editar_segmentacion/asignar_viviendas_a_segmento.sql
+++ editar_segmentacion/asignar_viviendas_a_segmento.sql
@@ -2,13 +2,14 @@
 SELECT indec.asignar_viviendas_a_segmento('e0960', 'ARRAY [594, 595, 596]', 10006);
 */
 
-drop indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint);
+drop function indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint);
 CREATE or replace FUNCTION indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint) 
 RETURNS integer AS $$ 
 declare cuantas integer;
 begin
 execute '
 select count(*)
+from ' || esquema || '.segmentacion
 where listado_id = any (' || viviendas || ')
 ' into cuantas;
 
