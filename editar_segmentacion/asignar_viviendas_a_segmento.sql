f941b6e0a0e503ea5fb1e64290dd8d586cc0e2fe
Hernán Alperin <alpe@alum.mit.edu>
2021-12-02 00:09:59 -0300

    usa raise exception si no coinciden las del array con las que pudo hacer update

--- editar_segmentacion/asignar_viviendas_a_segmento.sql
+++ editar_segmentacion/asignar_viviendas_a_segmento.sql
@@ -5,13 +5,19 @@ SELECT indec.asignar_viviendas_a_segmento('e0960', 'ARRAY [594, 595, 596]', 1000
 drop function indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint);
 CREATE or replace FUNCTION indec.asignar_viviendas_a_segmento(esquema text, viviendas text, segmento_id bigint) 
 RETURNS integer AS $$ 
-declare cuantas integer;
+declare 
+cuantas_updated integer;
+cuantas_viviendas integer;
 begin
+execute 'select cardinality(' || viviendas || ');' into cuantas_viviendas;
 execute '
 update ' || esquema || '.segmentacion 
 set segmento_id = ''' || segmento_id || '''
 where listado_id = any (' || viviendas || ')
 ';
-get diagnostics cuantas = row_count;
-return cuantas;
+get diagnostics cuantas_updated = row_count;
+if not (cuantas_updated = cuantas_viviendas) then
+  RAISE EXCEPTION 'difieren la cantidad de viviendas del array (%) de las que pudo enontrar en esquema.segmentación: %.', cuantas_viviendas, cuantas_updated;
+end if;
+return cuantas_updated;
 END $$ LANGUAGE plpgsql;
