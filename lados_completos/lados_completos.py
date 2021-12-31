1b598dd46239213eee4e0ad9c46980fde7b46218
Hernán Alperin <alpe@alum.mit.edu>
2021-12-31 07:52:17 -0300

    fix bug. usar la function conectados no anda

--- lados_completos/lados_completos.py
+++ lados_completos/lados_completos.py
@@ -420,7 +420,7 @@ for prov, dpto, frac, radio in radios:
             # soluciones iniciales
             soluciones_iniciales = []
             # iniciando de un extremo de la red de segmentaciones: segmento único igual a todo el radio
-            if 'usar_todos_juntos' in sys.argv and conectados([componentes]):
+            if 'usar_todos_juntos' in sys.argv:
               todos_juntos = [componentes]
               soluciones_iniciales.append(todos_juntos)
               # iniciando del otro extremo de la red de segmentaciones: un segmento por manzana
