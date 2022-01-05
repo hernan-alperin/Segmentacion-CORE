5f27b131062b017d6156d2fd009fe780b2c71b17
Hernán Alperin <alpe@alum.mit.edu>
2022-01-05 05:55:16 -0300

    agrega function indec.segmentar_equilibrado_ffrr circunscripta a un solo radio

--- manzanas_independientes/segmentar_equilibrado.sql
+++ manzanas_independientes/segmentar_equilibrado.sql
@@ -103,4 +103,96 @@ end;
 $function$
 ;
 
+create or replace function 
+indec.segmentar_equilibrado_ffrr(esquema text, _frac integer, _radio integer, deseado integer)
+    returns integer
+    language plpgsql volatile
+    set client_min_messages = error
+as $function$
+
+begin
+
+execute '
+with 
+parametros as (
+    select ' || deseado || '::float as deseado),
+listado as (select * from "' || esquema || '".listado),
+listado_sin_nulos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr,
+    coalesce(sector,'''') sector, coalesce(edificio,'''') edificio, coalesce(entrada,'''') entrada,
+    coalesce(piso, '''') piso, coalesce(CASE WHEN orden_reco='''' THEN NULL ELSE orden_reco END,''0'')::integer orden_reco
+    from listado
+    where frac = ' || _frac || ' and ' radio = ' || _radio  || ' 
+    ),
+
+casos as (
+    select prov, dpto, codloc, frac, radio, mza,
+           count(*) as vivs,
+           ceil(count(*)/deseado) as max,
+           greatest(1, floor(count(*)/deseado)) as min
+    from listado_sin_nulos, parametros
+    group by prov, dpto, codloc, frac, radio, mza, deseado
+    order by prov, dpto, codloc, frac, radio, mza, deseado
+    ),
+
+deseado_manzana as (
+    select prov, dpto, codloc, frac, radio, mza, vivs,
+        case when abs(vivs/max - deseado)
+            < abs(vivs/min - deseado) then max
+        else min end as segs_x_mza
+    from casos, parametros
+    ),
+
+pisos_abiertos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer,
+        row_number() over w as row, rank() over w as rank
+    from listado_sin_nulos
+    window w as (
+        partition by prov, dpto, codloc, frac, radio, mza
+        order by lado::integer, orden_reco::integer)
+    ),
+asignacion_segmentos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer,
+        floor((rank - 1)*segs_x_mza/vivs) + 1 as sgm_mza, rank
+    from deseado_manzana
+    join pisos_abiertos
+    using (prov, dpto, codloc, frac, radio, mza)
+    ),
+asignacion_segmentos_pisos_enteros as (
+    select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, min(sgm_mza) as sgm_mza
+    from asignacion_segmentos
+    group by prov, dpto, codloc, frac, radio, mza, lado,
+        nrocatastr, sector, edificio, entrada, piso
+    ),
+segmento_id_en_mza as (
+  select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer,
+    sgm_mza
+  from listado_sin_nulos
+  join asignacion_segmentos_pisos_enteros
+  using (prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso)
+  ),
+
+segmentos_id as (
+    select 
+        -- row_number() over (order by dpto, frac, radio, mza, sgm_mza) 
+        nextval(''"' || esquema || '".segmentos_seq'')
+        as segmento_id,
+        prov, dpto, codloc, frac, radio, mza, sgm_mza
+    from segmento_id_en_mza
+    group by prov, dpto, codloc, frac, radio, mza, sgm_mza
+    order by prov, dpto, codloc, frac, radio, mza, sgm_mza
+    )
+
+update "' || esquema || '".segmentacion sgm
+set segmento_id = j.segmento_id
+from (segmentos_id
+join segmento_id_en_mza
+using (prov, dpto, codloc, frac, radio, mza, sgm_mza)) j
+where listado_id = j.id
+';
+return 1;
+end;
+$function$
+;
+
         
