69bec0aaa7111ec2605eb6e640431b906a93f6e4
Hernán Alperin <alpe@alum.mit.edu>
2022-01-17 09:21:23 -0300

    rama reportes para local

--- reportes/estadisticas_avance.sql
+++ reportes/estadisticas_avance.sql
@@ -5,13 +5,13 @@ autor: -h
 fecha: 2022-01-17 Lu
 */
 
-with localidades as (
+with esquemas as (
   select schema_name as esquema
   from information_schema.schemata 
   where schema_name similar to 'e[0-9]{8}'),
 provs_localidades_conteo as (
   select substr(esquema,2,2) prov, count(*) as localidades
-  from localidades
+  from esquemas
   group by substr(esquema,2,2)),
 covers as (
   select substr(table_schema,2,2) prov, count(*) as covers
@@ -23,23 +23,33 @@ listados as (
   from information_schema.tables
   where table_schema similar to 'e[0-9]{8}' and table_name = 'listado'
   group by substr(table_schema,2,2)),
-/*
 provincias as (
   select codigo prov, nombre provincia
   from public.provincia),
-*/
+locs2010 as (
+  select substr(codigo,1,2) prov, count(*) locs2010
+  from public.localidad
+  group by substr(codigo,1,2)),
+radios2010 as (
+  select substr(codigo,1,2) prov, count(*) radios2010
+  from public.radio
+  group by substr(codigo,1,2)),
 estadisticas as (
-  select prov, '' provincia, 
-    localidades, covers, c1s
+  select prov, provincia, 
+    localidades, covers, c1s, locs2010, radios2010
   from provs_localidades_conteo
   natural full join covers
   natural full join listados
---  natural full join provincias
+  natural full join provincias
+  natural full join locs2010
+  natural full join radios2010
 )
-select prov, provincia, localidades, covers, c1s
+select prov, provincia, --localidades, covers, c1s, 
+  locs2010, radios2010
 from estadisticas
 union
-select '', 'total país', sum(localidades), sum(covers), sum(c1s)
+select '', 'total país', --sum(localidades), sum(covers), sum(c1s), 
+  sum(locs2010), sum(radios2010)
 from estadisticas
 order by prov
 ;
