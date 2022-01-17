e3db5baf54045f0fd16d52abcf06dd975aed82ea
Hernán Alperin <hernan.alperin@gmail.com>
2022-01-17 07:30:18 -0300

    ERROR:  permiso denegado a la tabla provincia
    en PRODSEG

--- reportes/estadisticas_avance.sql
+++ reportes/estadisticas_avance.sql
@@ -23,11 +23,14 @@ listados as (
   from information_schema.tables
   where table_schema similar to 'e[0-9]{8}' and table_name = 'listado'
   group by substr(table_schema,2,2)),
+/*
 provincias as (
   select codigo prov, nombre provincia
   from public.provincia),
+*/
 estadisticas as (
-  select prov, provincia, localidades, covers, c1s
+  select prov, '' provincia, 
+    localidades, covers, c1s
   from provs_localidades_conteo
   natural full join covers
   natural full join listados
@@ -42,8 +45,40 @@ order by prov
 ;
 
 
-
 /*
 2022-01-17  7:30
+psql -h 10.70.80.82 UATSEG -U halperin
+UATSEG=> \i estadisticas_avance.sql
+ prov |            provincia            | localidades | covers | c1s
+------+---------------------------------+-------------+--------+------
+      | total país                      |        1117 |   1095 | 1113
+ 02   | Ciudad Autónoma de Buenos Aires |          15 |      9 |   15
+ 06   | Buenos Aires                    |             |        |
+ 10   | Catamarca                       |         116 |    116 |  116
+ 14   | Córdoba                         |         452 |    450 |  450
+ 18   | Corrientes                      |          41 |     41 |   41
+ 22   | Chaco                           |             |        |
+ 26   | Chubut                          |          46 |     46 |   46
+ 30   | Entre Ríos                      |           2 |      2 |    2
+ 34   | Formosa                         |          34 |     33 |   34
+ 38   | Jujuy                           |          48 |     43 |   46
+ 42   | La Pampa                        |          22 |     22 |   22
+ 46   | La Rioja                        |          17 |     16 |   17
+ 50   | Mendoza                         |          16 |     10 |   16
+ 54   | Misiones                        |          36 |     35 |   36
+ 58   | Neuquén                         |          23 |     23 |   23
+ 62   | Río Negro                       |           9 |      9 |    9
+ 66   | Salta                           |         158 |    158 |  158
+ 70   | San Juan                        |           7 |      7 |    7
+ 74   | San Luis                        |          15 |     15 |   15
+ 78   | Santa Cruz                      |             |        |
+ 82   | Santa Fe                        |           4 |      4 |    4
+ 86   | Santiago del Estero             |           1 |      1 |    1
+ 90   | Tucumán                         |          49 |     49 |   49
+ 94   | Tierra del Fuego                |           6 |      6 |    6
+(25 rows)
+
+PRODSEG=> \i estadisticas_avance.sql
+psql:estadisticas_avance.sql:42: ERROR:  permiso denegado a la tabla provincia
 
 */
