e32e3862265adb53edc21873ade4f5f41b972d9e
Neptuso <neptuso@gmail.com>
2022-03-08 18:29:10 -0300

    Agrega RAISE NOTICE comenta nivel de error y algo mas

--- segmentos_excedidos.sql
+++ segmentos_excedidos.sql
@@ -1,11 +1,12 @@
+
 /*
 titulo: segmentos_excedidos.sql
 descripción: función que devuelve una lista de los segmentos excedidos en el radio
-
 autor: -h
 fecha:2020-11
 */
 
+drop function if exists indec.segmentos_excedidos(esquema text, umbral integer);
 create or replace function
 indec.segmentos_excedidos(esquema text, umbral integer)
     returns table (segmento_id bigint)
@@ -17,7 +18,7 @@ begin
 
 return query
 execute '
-select segmento_id
+select segmento_id::bigint
 from "' || esquema || '".listado
 join "' || esquema || '".segmentacion
 on listado_id = listado.id
@@ -28,9 +29,10 @@ end;
 $function$
 ;
 
+drop function if exists indec.segmentos_excedidos_ffrr(esquema text, ff integer, rr integer, umbral integer);
 create or replace function
 indec.segmentos_excedidos_ffrr(esquema text, ff integer, rr integer, umbral integer)
-    returns table (segmento_id bigint)
+    returns table (segmento_id bigint, cantidad bigint)
     language plpgsql volatile
     set client_min_messages = error
 as $function$
@@ -39,7 +41,7 @@ begin
 
 return query
 execute '
-select segmento_id
+select segmento_id::bigint, count(indec.contar_vivienda(tipoviv)) cantidad
 from "' || esquema || '".listado
 join "' || esquema || '".segmentacion
 on listado_id = listado.id
@@ -56,26 +58,30 @@ create or replace function
 indec.segmentar_excedidos_ffrr(esquema text, ff integer, rr integer, umbral integer, deseado integer)
     returns integer
     language plpgsql volatile
-    set client_min_messages = error
+    //set client_min_messages = error
 as $function$
-
 declare
   excedidos record;
+  segmentar_equilibrados_sql character varying;
+  myquery_result character varying;
 begin
+ RAISE NOTICE 'Segmentar Excedidos FF RR';
 for excedidos in
-  select segmento_id from indec.segmentos_excedidos_ffrr(esquema, ff, rr, umbral)
+  select segmento_id, cantidad from indec.segmentos_excedidos_ffrr(esquema, ff, rr, umbral)
 loop
-  execute 'select indec.segmentar_listado_equilibrado(''' || esquema || ''', 
+  segmentar_equilibrados_sql :=  'select indec.segmentar_listado_equilibrado(''' || esquema || ''', 
   ''select * from "' || esquema || '".listado
   join "' || esquema || '".segmentacion
   on listado_id = listado.id
   where segmento_id = ' || excedidos.segmento_id::text || ''', 
-  '' mza::integer, lado::integer, orden_reco::integer ''::text, ' || deseado || ')';
+  '' mza::integer, lado::integer, orden_reco::integer ''::text, 
+  least(' || deseado || ', ' || excedidos.cantidad || '/2))';
+  execute segmentar_equilibrados_sql into myquery_result;
+RAISE NOTICE 'SQL % .',segmentar_equilibrados_sql;
+  
 end loop;
 
 return 1;
 end;
 $function$
 ;
-
-
